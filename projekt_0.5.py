b81850ae930e93a8c6ae8982b996e632271f5d15
Spoldma <sander.poldma@gmail.com>
2022-12-06 14:07:56 +0200

    nimed + võitja(mingil määral)

--- projekt_0.5.py
+++ projekt_0.5.py
@@ -13,13 +13,13 @@ def kaardipakk():
     return kaardipakk
 
 # n arvule mängijatele käte genereerimine, formaadis ([käsi1], [käsi2], ...), välja jagatud kaardid eemaldatakse kaardipakist
-def käed(mängijad, kaardipakk):
+def käed(mängijad, kaardipakk, nimed):
     käed = []
     for i in range(mängijad):
         x = random.sample(kaardipakk, 2)
         kaardipakk = eemalda(kaardipakk, x)
         käed.append(x)
-        print('Mängija', i + 1, 'käsi on:', käed[i])
+        print(nimed[i], 'sai kätte:', käed[i])
     print('\n')
     return käed, kaardipakk
 
@@ -232,19 +232,30 @@ def kontrolli_high_card(käsi):
 # leiab võitja (lõpetamata)
 def leia_võitja(käed):
     suurus = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-    uus_järjestus = []
-    if len(käed) == 2:
-        if käed[0][0] > käed[1][0]:
-            print('1. mängija võitis')
-        elif käed[0][0] < käed[1][0]:
-            print('2. mängija võitis')
-        elif käed[0][0] == käed[1][0]:
-            if suurus.index(käed[0][1]) > suurus.index(käed[1][1]):
-                print('1. mängija võitis')
-            elif suurus.index(käed[0][1]) < suurus.index(käed[1][1]):
-                print('2. mängija võitis')
-            else:
-                print('Mäng jäi viiki')
+    võitjad = []
+    suurim = 0
+    for i in käed:
+        if i[0] > suurim:
+            suurim = i[0]
+    for i in käed:
+        if i[0] == suurim:
+            võitjad.append(i)
+
+    if len(võitjad) == 1:
+        print('Mängu võitis: mängija', str(käed.index(võitjad[0]) + 1))
+    elif len(võitjad) == 2:
+        if võitjad[0][1] == võitjad[1][1]:
+            print()
+
+def mängijate_nimed(mängijate_arv):
+    jär = []
+    for i in range(mängijate_arv):
+        tekst = 'Sisesta' + str(i+1) + '. mängija nimi: '
+        nimi = input(tekst)
+        jär.append(nimi)
+    return jär
+        
+
             
         
         
@@ -254,10 +265,13 @@ mängijate_arv = input('Sisesta mängijate arv: ')
 mängijate_arv = int(mängijate_arv)
 mängijate_arv = kontrolli_mängijate_arvu(mängijate_arv, 5)
 
+nimed = mängijate_nimed(mängijate_arv)
+#nimed = [1, 2, 3, 4, 5]
+
 # Segatakse kaardipakk
 kaardipakk = kaardipakk()
 # Jagatakse välja käed
-käed, kaardipakk = käed(mängijate_arv, kaardipakk)
+käed, kaardipakk = käed(mängijate_arv, kaardipakk, nimed)
 
 # Jagatakse välja laud
 laud = []
@@ -272,43 +286,43 @@ j = -1
 for i in käed:
     j += 1
     if kontrolli_royal_flush(i) == True:
-        print('Mängijal', j + 1, 'on Royal FLush')
+        print('Mängijal', nimed[j], 'on Royal FLush')
         käed[j] = (10, 'A')
     
     elif kontrolli_straight_flush(i) == True:
-        print('Mängijal', j + 1, 'on Straight Flush')
+        print('Mängijal', nimed[j], 'on Straight Flush')
         käed[j] = (9, kontrolli_straight_flush(i)[1])
     
     elif kontrolli_four_of_a_kind(i)[0] == True:
-        print('Mängijal', j + 1, 'on Four of a Kind')
+        print('Mängijal', nimed[j], 'on Four of a Kind')
         käed[j] = (8, kontrolli_four_of_a_kind(i)[1])
         
     elif kontrolli_straight(i)[0] == True:
-        print('Mängijal', j + 1, 'on Straight')
+        print('Mängijal', nimed[j], 'on Straight')
         käed[j] = (7, kontrolli_straight(i)[1])
     
     elif kontrolli_full_house(i)[0] == True:
-        print('Mängijal', j + 1, 'on Full House')
+        print('Mängijal', nimed[j], 'on Full House')
         käed[j] = (6, kontrolli_full_house(i)[1], kontrolli_full_house(i)[2])
     
     elif kontrolli_flush(i)[0] == True:
-        print('Mängijal', j + 1, 'on Flush')
+        print('Mängijal', nimed[j], 'on Flush')
         käed[j] = (5, kontrolli_flush(i)[1])
     
     elif kontrolli_three_of_a_kind(i)[0] == True:
-        print('Mängijal', j + 1, 'on Three of a Kind')
+        print('Mängijal', nimed[j], 'on Three of a Kind')
         käed[j] = (4, kontrolli_three_of_a_kind(i)[1])
     
     elif kontrolli_two_pairs(i)[0] == True:
-        print('Mängijal', j + 1, 'on Kaks Paari')
+        print('Mängijal', nimed[j], 'on Kaks Paari')
         käed[j] = (3, kontrolli_two_pairs(i)[1], kontrolli_two_pairs(i)[2])
     
     elif kontrolli_pair(i)[0] == True:
-        print('Mängijal', j + 1, 'on Paar')
+        print('Mängijal', nimed[j], 'on Paar')
         käed[j] = (2, kontrolli_pair(i)[1])
     
     elif kontrolli_high_card(i)[0] == True:
-        print('Mängijal', j + 1, 'on High Card')
+        print('Mängijal', nimed[j], 'on High Card')
         käed[j] = (1, kontrolli_high_card(i)[1])
 print(käed)
 
